9e23696fcc5aac4d7d2572c04b46b44fe3ddd485
Ryan Sharafuddin <trashnapps@gmail.com>
2025-12-11 03:08:24 -0500

    Update help text to reflect the fact that capitulate mode is currently not implemented

--- controller.py
+++ controller.py
@@ -112,7 +112,7 @@ def f_name_from_id(identity):
 
 def make_solver(problem: Problem):
     """ Makes a solver and solve()s the problem. """
-    # if(args.capitulate): # args.capitulate should really be passed in as an argument to this function. At any ratd, capitulate solver isn't working currently anyway.
+    # if(args.capitulate): # args.capitulate should really be passed in as an argument to this function. At any rate, capitulate solver isn't working currently anyway.
         # s = Solver_Capitulate(problem)
     if(problem.mode == NIGHTMARE):
         s = Solver_Nightmare(problem)
@@ -315,7 +315,7 @@ def make_parser():
     parser.add_argument(
         "--capitulate","-c",
         action="store_true",
-        help="Give up on being perfect. Choosing this turns on --no_pickles."
+        help="Note: capitulate mode is currently not implemented, so don't use this option. But, when it is implemented, this help text will say: Give up on being perfect. Choosing this turns on --no_pickles."
     )
     parser.add_argument(
         "--new_problem", "-n",
