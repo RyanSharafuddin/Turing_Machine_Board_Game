a6d11f3ea15ccbb7fa3aa89115cb24250ad9119f
Ryan Sharafuddin <trashnapps@gmail.com>
2026-09-16 16:22:47 -0400

    Added a dev-requirements.in and .txt file for syncing with requirements that are needed for testing, but not for running. Also in process of updating tests.

--- Tests/test_problems.py
+++ Tests/test_problems.py
@@ -2,28 +2,28 @@ import controller
 from src.core.definitions import console as console
 from src.core.solver import Solver as Solver
 from src.problems.problems import get_best_time as get_best_time
+import src.problems.problems as problems
 # problems, solver
 # import math
 # NOTE: run .venv/bin/pytest --capture=tee-sys to see code output in real-time, rather than having it all captured.
-class TestProblems:
-    @staticmethod
-    def get_problem_and_compare_output(p_id, expected_cost):
-        """
-        Assert that the solver produces the same evaluation cost as before, and within a reasonable amount of time.
-        """
-        p = controller.get_requested_problem(p_id=p_id)
-        console.print(f"\nNow testing problem {p.identity}")
-        s : Solver = controller.get_or_make_solver(p, no_pickles=True, force_overwrite=False)[0]
-        assert (s.expected_cost == expected_cost)
-        previous_best_time = get_best_time(p)
-        assert (s.seconds_to_solve <= min(previous_best_time + 10, previous_best_time * 1.15))
-        console.print(f"Previous best time: {previous_best_time}. Time this run: {s.seconds_to_solve}.")
+def get_problem_and_compare_output(p_id, expected_cost):
+    """
+    Assert that the solver produces the same evaluation cost as before, and within a reasonable amount of time.
+    """
+    p = problems.get_requested_problem(p_id=p_id)
+    console.print(f"\nNow testing problem {p.identity}")
+    s : Solver = controller.get_or_make_solver(p, no_pickles=True, force_overwrite=False)[0]
+    assert (s.expected_cost == expected_cost) # BUG: use approximately equal floating point instead. Or switch to using Fractions.
+    previous_best_time = get_best_time(p)
+    assert (s.seconds_to_solve <= min(previous_best_time + 10, previous_best_time * 1.15))
+    console.print(f"Previous best time: {previous_best_time}. Time this run: {s.seconds_to_solve}.")
 
+class TestProblems:
     def test_zero_query(self):
         """
         Tests that the zero_query problem takes zero queries and rounds to solve.
         """
-        TestProblems.get_problem_and_compare_output("b63yrw4", (0,0))
+        get_problem_and_compare_output("b63yrw4", (0,0))
 
     def test_f43(self):
         TestProblems.get_problem_and_compare_output("f435fe", (2.1581920903954797, 5.977401129943503))
